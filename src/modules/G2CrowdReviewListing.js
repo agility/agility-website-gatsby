ac9420f0bcc7f7c02db599fc345d0769ab3e266b
Joel Varty <joelvarty@outlook.com>
2020-03-20 15:46:55 -0400

    fixed g2 crowd issue

--- src/modules/G2CrowdReviewListing.js
+++ src/modules/G2CrowdReviewListing.js
@@ -1,21 +1,49 @@
-import React from 'react';
+import React, {  useEffect, useState } from 'react';
 import { renderHTML } from '../agility/utils'
 
 import './G2CrowdReviewListing.scss'
 
 const G2CrowdReviewListing = ({ item }) => {
 
+	const [state, setState] = useState({
+        loaded: false
+	})
+
+	useEffect(() => {
+
+		//load the g2 script...
+		if (typeof window === 'undefined') return;
+		if (state.loaded) return;
+
+		let script = document.createElement("script")
+		script.src = "https://apps.elfsight.com/p/platform.js"
+		script.async = true
+		document.body.appendChild(script)
+
+		setState({
+			loaded: true,
+		})
+
+
+
+	}, []);
+
 	return (
 
-		<section className="g2-review-listing features p-w">
+		<section className="g2-review-listing">
 
 			<h2 className="title-component">{item.customFields.heading}</h2>
 			<p className="intro">{item.customFields.subHeading}</p>
 			<div>
-				<div className="container-my">
-					<div className="row-my">
+				<div className="g2-review-panel">
+
+					{ (typeof window !== 'undefined') &&
 						<div className="g2-review-widget" dangerouslySetInnerHTML={renderHTML(item.customFields.widgetCode)}></div>
-					</div>
+					}
+					{ (typeof window === 'undefined') &&
+						<div className="g2-review-widget"></div>
+					}
+
 				</div>
 			</div>
 		</section>
