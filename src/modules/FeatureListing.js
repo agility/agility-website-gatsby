7b1e776bd7d235cb3ad6bc55df83c0d6e3536467
Joel Varty <joelvarty@outlook.com>
2020-03-20 16:47:27 -0400

    added missing modules

---
+++ src/modules/FeatureListing.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const FeatureListing = ({ item }) => {
+
+	return (
+		<section className="container">
+			FeatureListing
+        </section>
+	);
+}
+
+export default FeatureListing;
---
+++ src/modules/GatedDownload.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const GatedDownload = ({ item }) => {
+
+	return (
+		<section className="container">
+			GatedDownload
+        </section>
+	);
+}
+
+export default GatedDownload;
