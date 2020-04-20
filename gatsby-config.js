181e998ded286e1ee8843fdcb59d3a3e5a519667
Joel Varty <joelvarty@outlook.com>
2020-04-20 10:22:18 -0400

    updated gtm

--- gatsby-config.js
+++ gatsby-config.js
@@ -60,22 +60,10 @@ module.exports = {
 		{
 			resolve: "gatsby-plugin-google-tagmanager",
 			options: {
-				id: "GTM-KQ22VX2",
-
-				// Include GTM in development.
-				// Defaults to false meaning GTM will only be loaded in production.
-				includeInDevelopment: true,
-
-				// datalayer to be set before GTM is loaded
-				// should be an object or a function that is executed in the browser
-				// Defaults to null
-				//defaultDataLayer: { platform: "gatsby" },
-
-				// Specify optional GTM environment details.
-				// gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
-				// gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
-				// dataLayerName: "YOUR_DATA_LAYER_NAME",
+				id: "GTM-NJW8WMX", //Agilitycms.com - Gatsby Container
+				includeInDevelopment: false
 			},
 		},
+
 	],
 }
