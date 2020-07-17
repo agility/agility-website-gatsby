308d03bcbb15680059d416fb14279d1b2553033b
Joel Varty <joelvarty@outlook.com>
2020-07-17 14:55:50 -0400

    added pricing table

---
+++ src/modules/PricingTable.js
@@ -0,0 +1,99 @@
+
+import React from 'react';
+import { graphql, StaticQuery } from "gatsby"
+
+
+export default props => (
+	<StaticQuery
+		query={graphql`
+		query PricingTableQuery {
+
+			allAgilityBlogPost(filter: {properties: {referenceName: {eq: "blogposts"}}}, sort: {fields: customFields___date, order: DESC}) {
+			  nodes {
+				contentID
+				customFields {
+				  date(formatString: "MMMM D, YYYY")
+				  excerpt
+				  title
+				  uRL
+				  postImage {
+					url
+					label
+				  }
+				}
+				category {
+				  customFields {
+					title
+				  }
+				}
+				author {
+				  customFields {
+					image {
+					  url
+					  label
+					}
+					title
+				  }
+				}
+				tags {
+					contentID
+					customFields {
+						title
+					}
+				}
+			  }
+			}
+		  }
+        `}
+		render={queryData => {
+
+			//filter out only those logos that we want...
+			let posts = queryData.allAgilityBlogPost.nodes;
+
+			// //filter by ref name
+			const referenceName = props.item.customFields.tableValues.referencename
+
+			console.log("REF NAME", referenceName)
+
+			// if (props.dynamicPageItem && props.dynamicPageItem.properties.definitionName === "BlogTag") {
+			// 	const tagContentID = props.dynamicPageItem.contentID;
+
+			// 	posts = posts.filter(p => {
+			// 		if (!p.tags || !(p.tags.length > 0)) return false;
+			// 		const index = p.tags.findIndex(t => { return parseInt(t.contentID) === parseInt(tagContentID); });
+			// 		return index >= 0;
+			// 	});
+			// }
+
+			// //adjust the excerpt
+			// posts.forEach(p => {
+			// 	let excerpt = p.customFields.excerpt;
+			// 	if (excerpt) {
+			// 		p.customFields.excerpt = StringUtils.stripHtml(excerpt, 200);
+			// 	}
+
+			// 	p.url = "/resources/posts/" + p.customFields.uRL;
+			// });
+
+			const viewModel = {
+				item: props.item,
+				posts: posts
+			}
+			return (
+				<PricingTable {...viewModel} />
+			);
+		}
+		}
+	/>
+)
+
+
+const PricingTable = ({ item, posts }) => {
+	console.log("PRICING TABLE", item)
+
+	return (
+		<section>PRICING TABLE</section>
+	)
+
+
+}
