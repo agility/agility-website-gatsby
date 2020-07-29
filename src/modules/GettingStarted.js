220d7164ac393a0b1266bfcf92f2b371fea0a08e
Joel Varty <joelvarty@outlook.com>
2020-07-29 11:51:48 -0400

    implemented scroll to position

--- src/modules/GettingStarted.js
+++ src/modules/GettingStarted.js
@@ -1,8 +1,7 @@
-import React, {useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { graphql, StaticQuery } from "gatsby"
 import { renderHTML } from '../agility/utils'
-
-import VisibilitySensor from 'react-visibility-sensor'
+import smoothscroll from 'smoothscroll-polyfill';
 
 import "./GettingStarted.scss"
 import './RichTextArea.scss'
@@ -66,18 +65,30 @@ export default props => (
 
 const GettingStarted = ({ item, gettingStartedItems }) => {
 	const [persona, setPersona] = useState(null)
+
 	item = item.customFields;
 
+	smoothscroll.polyfill();
 
 	const setCurrentPersona = (personaValue) => {
 		if (typeof window !== 'undefined'
 			&& window.localStorage
 			&& window.localStorage.setItem
-			) {
-				window.localStorage.setItem("persona", personaValue)
-			}
+		) {
+			window.localStorage.setItem("persona", personaValue)
+		}
 
 		setPersona(personaValue)
+
+		setTimeout(function() {
+			//scroll to the selected element
+			let el = document.getElementById(`item-content-${personaValue}`)
+			let scrollToPos = el.offsetTop;
+			window.scrollTo({top: scrollToPos, behavior: "smooth"})
+		}, 250);
+
+
+
 	}
 
 	useEffect(() => {
@@ -85,7 +96,7 @@ const GettingStarted = ({ item, gettingStartedItems }) => {
 		if (typeof window === 'undefined'
 			|| !window.localStorage
 			|| !window.localStorage.getItem
-			) return;
+		) return;
 
 		const localPersona = window.localStorage.getItem("persona");
 
@@ -93,7 +104,7 @@ const GettingStarted = ({ item, gettingStartedItems }) => {
 	}, [])
 
 	let scrollTestTimeout = -1;
-	let scrollState= {left: -1, width: -1}
+	let scrollState = { left: -1, width: -1 }
 	const cardScroll = (e, x) => {
 		const el = e.target;
 
@@ -124,45 +135,46 @@ const GettingStarted = ({ item, gettingStartedItems }) => {
 		<section className="getting-started">
 			<h2 className="title-component">{item.heading}</h2>
 			<div className="container-my">
-
 					<div className="cards" onScroll={cardScroll}>
-					{ gettingStartedItems.map(g => (
+						{gettingStartedItems.map(g => (
 
-						<button key={`gsc-${g.contentID}`} className={`card ${g.contentID === selectedID ? "selected" : ""}`} onClick={() => {setCurrentPersona(g.customFields.personaCookieValue, g.contentID )}}>
-							<div className="card-image" style={{backgroundImage:`url(${g.customFields.cardImage.url}?w=400)`}}>&nbsp;</div>
-							<div className="card-title">
-								<h3>{g.customFields.title}</h3>
-							</div>
-						</button>
-					))}
+							<button key={`gsc-${g.contentID}`} className={`card ${g.contentID === selectedID ? "selected" : ""}`}
+								onClick={() => { setCurrentPersona(g.customFields.personaCookieValue) }}>
+								<div className="card-image" style={{ backgroundImage: `url(${g.customFields.cardImage.url}?w=400)` }}>&nbsp;</div>
+								<div className="card-title">
+									<h3>{g.customFields.title}</h3>
+								</div>
+							</button>
+						))}
 					</div>
 
 
-				<div className="item-container">
-				{ gettingStartedItems.map(g => (
-					<div className={`item ${g.contentID === selectedID ? "selected" : ""}`} key={`gsi-${g.contentID}`}>
-						<div className="item-flex">
+				<div className="item-container" >
 
-							<div className="item-image"><img src={`${g.customFields.image.url}?w=800`} alt={g.customFields.image.label} /></div>
-							<div className="item-content">
-								<h3>{g.customFields.heading}</h3>
-								<div className="rich-text" dangerouslySetInnerHTML={renderHTML(g.customFields.content)}></div>
+					{gettingStartedItems.map(g => (
+						<div className={`item ${g.contentID === selectedID ? "selected" : ""}`} key={`gsi-${g.contentID}`}>
+							<div className="item-flex">
 
-								<div className="item-ctas">
-									<a className="btn primary-cta" href={g.customFields.primaryButton.href} target={g.customFields.primaryButton.target}>{g.customFields.primaryButton.text}</a>
-									{ g.customFields.secondaryButton &&
-										<a className="btn secondary-cta" href={g.customFields.secondaryButton.href} target={g.customFields.secondaryButton.target}>{g.customFields.secondaryButton.text}</a>
-									}
-								</div>
-								{/* { g.customFields.thirdCTA &&
+								<div className="item-image"><img src={`${g.customFields.image.url}?w=800`} alt={g.customFields.image.label} /></div>
+								<div className="item-content" id={`item-content-${g.customFields.personaCookieValue}`}>
+									<h3>{g.customFields.heading}</h3>
+									<div className="rich-text" dangerouslySetInnerHTML={renderHTML(g.customFields.content)}></div>
+
+									<div className="item-ctas">
+										<a className="btn primary-cta" href={g.customFields.primaryButton.href} target={g.customFields.primaryButton.target}>{g.customFields.primaryButton.text}</a>
+										{g.customFields.secondaryButton &&
+											<a className="btn secondary-cta" href={g.customFields.secondaryButton.href} target={g.customFields.secondaryButton.target}>{g.customFields.secondaryButton.text}</a>
+										}
+									</div>
+									{/* { g.customFields.thirdCTA &&
 										<div className="third-cta-row">
 											<a className="third-cta" href={g.customFields.thirdCTA.href} target={g.customFields.thirdCTA.target}>{g.customFields.thirdCTA.text}</a>
 										</div>
 									} */}
-							</div>
+								</div>
 
+							</div>
 						</div>
-					</div>
 					))}
 				</div>
 			</div>
