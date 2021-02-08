bb5a63eabf02a490618f7d3f5d0d2080f44a4e68
Joel Varty <joel@agilitycms.com>
2021-02-08 12:07:10 -0500

    tweaks to pricing page

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -165,10 +165,10 @@ const SEO = ({ page }) => {
 			<meta name="msapplication-wide310x150logo" content="https://static.agilitycms.com/favicon-web/mstile-310x150.png" />
 			<meta name="msapplication-square310x310logo" content="https://static.agilitycms.com/favicon-web/mstile-310x310.png" />
 
-			<link rel="preload" as="font" type="font/woff2" crossorigin="anonymous" href="https://static.agilitycms.com/layout/fonts/Mulisemibold.woff2" />
-			<link rel="preload" as="font" type="font/woff2" crossorigin="anonymous" href="https://static.agilitycms.com/layout/fonts/Muliextrabold.woff2" />
-			<link rel="preload" as="font" type="font/woff2" crossorigin="anonymous" href="https://static.agilitycms.com/layout/fonts/Mulibold.woff2" />
-			<link rel="preload" as="font" type="font/woff2" crossorigin="anonymous" href="https://static.agilitycms.com/layout/fonts/Muliregular.woff2" />
+			<link rel="preload" as="font" type="font/woff2" crossOrigin="anonymous" href="https://static.agilitycms.com/layout/fonts/Mulisemibold.woff2" />
+			<link rel="preload" as="font" type="font/woff2" crossOrigin="anonymous" href="https://static.agilitycms.com/layout/fonts/Muliextrabold.woff2" />
+			<link rel="preload" as="font" type="font/woff2" crossOrigin="anonymous" href="https://static.agilitycms.com/layout/fonts/Mulibold.woff2" />
+			<link rel="preload" as="font" type="font/woff2" crossOrigin="anonymous" href="https://static.agilitycms.com/layout/fonts/Muliregular.woff2" />
 
 			<link rel="stylesheet" href="/fonts.css" />
 
