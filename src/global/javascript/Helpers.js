12dbf41fb9e1aa6e07bcb0e1b78bd8e65c989dea
Joel Varty <joel@agilitycms.com>
2021-03-30 17:00:15 -0400

    cookie parse fix

--- src/global/javascript/Helpers.js
+++ src/global/javascript/Helpers.js
@@ -1,9 +1,9 @@
 const HelperFunc = {
-  animateScrollTop: (elementY, duration) => { 
+  animateScrollTop: (elementY, duration) => {
 		var startingY = window.pageYOffset;
 		var diff = elementY - startingY;
 		var start;
-	
+
 		// Bootstrap our animation - it will get called right before next frame shall be rendered.
 		window.requestAnimationFrame(function step(timestamp) {
 			if (!start) start = timestamp;
@@ -11,9 +11,9 @@ const HelperFunc = {
 			var time = timestamp - start;
 			// Get percent of completion in range [0, 1].
 			var percent = Math.min(time / duration, 1);
-	
+
 			window.scrollTo(0, startingY + diff * percent);
-	
+
 			// Proceed with animation as long as we wanted it to.
 			if (time < duration) {
 				window.requestAnimationFrame(step);
@@ -29,6 +29,8 @@ const HelperFunc = {
 		document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 	},
 	getCookie:(cname) => {
+		if (! document) return ""
+		if (! document.cookie) return "";
 		var name = cname + "=";
 		var ca = document.cookie.split(';');
 		for(var i = 0; i < ca.length; i++) {
