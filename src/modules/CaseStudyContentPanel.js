e72c4cd70efb7ce831e3264140e05a9b7da82cd7
Tu Tran <tu.tran@9thwonder.com>
2021-11-03 13:06:21 +0700

    Update logo customer in case study

--- src/modules/CaseStudyContentPanel.js
+++ src/modules/CaseStudyContentPanel.js
@@ -7,6 +7,8 @@ import Spacing from './Spacing'
 import { animationElementInnerComponent } from '../global/javascript/animation'
 import "./CaseStudyContentPanel.scss"
 import Helmet from 'react-helmet'
+import Lazyload from 'react-lazyload'
+import Helpers from '../global/javascript/Helpers'
 
 const CaseStudyContentPanel = ({ item, dynamicPageItem }) => {
 
@@ -33,9 +35,8 @@ const CaseStudyContentPanel = ({ item, dynamicPageItem }) => {
 	// let bgColor = caseStudy.brandBGColor;
 	// let fgColor = caseStudy.brandFGColor;
 
-
-
 	const metricsReferenceName = dynamicPageItem.customFields.metrics.referencename;
+	const customerLogo = caseStudy?.customerLogo?.url
 
 	//filter out only those logos that we want...
 	let metrics = query.allAgilityKeyValuePair.nodes.filter(m => {
@@ -71,6 +72,7 @@ const CaseStudyContentPanel = ({ item, dynamicPageItem }) => {
 			window.removeEventListener('scroll', scrollEventFunc)
 		}
 	}, [])
+	console.log('caseStudyyyyyyyyyyyyyyy', caseStudy)
 
 	return (
 		<>
@@ -86,6 +88,9 @@ const CaseStudyContentPanel = ({ item, dynamicPageItem }) => {
 							<div className="in-left d-flex flex-column h-100">
 								<div className={`d-table w-100 ${metrics.length ? '' : 'h-100'}`}>
 									<div className="d-table-cell align-middle last-mb-none">
+									{ customerLogo && <Lazyload offset={Helpers.lazyOffset} className="customer-logo">
+										<img src={customerLogo} className="bg transition-25"></img>
+									</Lazyload>}
 										<h1>{caseStudy.title}</h1>
 										<p>{caseStudy.excerpt}</p>
 									</div>
@@ -125,46 +130,6 @@ const CaseStudyContentPanel = ({ item, dynamicPageItem }) => {
 			</section>
 			<Spacing item={item} />
 		</>
-
-		// <section className="p-w case-study-content-panel" style={{ backgroundColor: bgColor }}>
-
-
-		// 	<div className="container-my">
-		// 		<div className="content-panel-flex">
-
-		// 			{caseStudy.imagePosition === 'left' &&
-
-		// 				<div className="start-image">
-		// 					{caseStudy.image && caseStudy.image != null &&
-		// 						<AgilityImage image={caseStudy.image} layout="constrained" width="500"   />
-		// 					}
-		// 				</div>
-
-		// 			}
-
-		// 			<div className="start-content">
-		// 				<div className="sc-inner">
-		// 					<div className="image">
-		// 						{caseStudy.customerLogo && caseStudy.customerLogo != null ? <img src={caseStudy.customerLogo.url} alt={caseStudy.customerLogo.label} /> : null}
-		// 					</div>
-		// 					<div style={{ color: fgColor }} dangerouslySetInnerHTML={renderHTML(caseStudy.contentPanelCopy)} />
-		// 				</div>
-		// 			</div>
-
-
-
-		// 			{caseStudy.imagePosition === 'right' &&
-
-		// 				<div className="start-image">
-		// 					<div className="image-inner">
-		// 						{caseStudy.image && caseStudy.image != null ? <img src={caseStudy.image.url + '?w=500&h=500'} alt={caseStudy.image.label} /> : null}
-		// 					</div>
-		// 				</div>
-
-		// 			}
-		// 		</div>
-		// 	</div>
-		// </section>
 	);
 }
 
