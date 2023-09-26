490a58aeff273a5f658d22634890a84b1ae8c8e2
Joel Varty <joel@agilitycms.com>
2023-09-26 15:58:47 -0400

    added stackbit

---
+++ .stackbit/cache/stackbit.config.2D3I4G52.cjs
@@ -0,0 +1,33 @@
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __export = (target, all) => {
+  for (var name in all)
+    __defProp(target, name, { get: all[name], enumerable: true });
+};
+var __copyProps = (to, from, except, desc) => {
+  if (from && typeof from === "object" || typeof from === "function") {
+    for (let key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(to, key) && key !== except)
+        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+  }
+  return to;
+};
+var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
+
+// stackbit.config.js
+var stackbit_config_exports = {};
+__export(stackbit_config_exports, {
+  default: () => stackbit_config_default
+});
+module.exports = __toCommonJS(stackbit_config_exports);
+var import_types = require("../../node_modules/@stackbit/types/dist/index.js");
+var stackbit_config_default = (0, import_types.defineStackbitConfig)({
+  stackbitVersion: "~0.6.0",
+  ssgName: "gatsby",
+  nodeVersion: "16"
+});
+// Annotate the CommonJS export names for ESM import in node:
+0 && (module.exports = {});
+//# sourceMappingURL=stackbit.config.2D3I4G52.cjs.map
---
+++ stackbit.config.js
@@ -0,0 +1,7 @@
+import { defineStackbitConfig } from '@stackbit/types'
+
+export default defineStackbitConfig({
+	stackbitVersion: '~0.6.0',
+	ssgName: 'gatsby',
+	nodeVersion: '16',
+})
